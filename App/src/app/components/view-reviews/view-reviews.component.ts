21463ffb79c9af01ce3e60427b0c620320ba459d
Tom Keech <45048917+mot667@users.noreply.github.com>
2020-12-02 16:17:20 -0500

    Can display reviews
    Admins can now see a list of reviews

---
+++ App/src/app/components/view-reviews/view-reviews.component.ts
@@ -0,0 +1,139 @@
+import { Component, OnInit } from '@angular/core';
+import { TutorialService } from '../../services/tutorial.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import { AlertDialogComponent } from '../alert-dialog/alert-dialog.component';
+import { MatDialog} from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Router} from "@angular/router";
+
+
+@Component({
+  selector: 'viewreviews',
+  templateUrl: './view-reviews.component.html',
+  styleUrls: ['./view-reviews.component.css']
+})
+export class ViewReviews implements OnInit {
+  timetables: any[] = [];
+  displayedColumns: string[] = ['ClassName', 'Subject', 'Review', 'timestamp'];
+  dataSource: any = new MatTableDataSource(this.timetables);
+
+  myControl = new FormControl();
+  options: string[] = [];
+  filteredOptions: Observable<string[]>;
+
+  constructor(private tutorialService: TutorialService,private dialog: MatDialog, private _snackBar: MatSnackBar,private router: Router) { 
+
+    
+  }
+
+  ngOnInit(){
+    var userEmail = localStorage.getItem('userEmail');
+    this.tutorialService.getReview({})
+    .subscribe(
+      response => {
+          console.log(response)
+        this.timetables = response;
+
+        response.forEach(element => {
+          this.options.push(element.className);
+        });
+        this.filteredOptions = this.myControl.valueChanges
+        .pipe(
+          startWith(''),
+          map(value => this._filter(value))
+        );
+
+
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+
+  onDelete() {
+    //console.log(this.myControl.value);
+    this.tutorialService.deleteCourse({data:this.myControl.value, userEmail: localStorage.getItem('userEmail')})
+    .subscribe(
+      response => {
+        console.log(response);
+        if(response.succes == false) {
+          const dialogRef = this.dialog.open
+          (AlertDialogComponent,{
+            data:{
+              message: `Schedule by the name ${this.myControl.value} dosen't exist`,
+              buttonText: {
+                cancel: 'OK'
+              }
+            },
+          });
+        } else if(response.succes == true) {
+          this.openSnackBar("Successfully Deleted","OK")
+          this.ngOnInit();
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
+  deleteAll() {
+    this.tutorialService.deleteAll({userEmail:localStorage.getItem('userEmail')})
+    .subscribe(
+      response => {
+        console.log(response);
+        if(response.success == true) {
+          this.openSnackBar("Successfully Deleted","OK")
+          this.ngOnInit();
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  onEdit(selectedTimetable) {
+    console.log(selectedTimetable);
+    localStorage.setItem('_id', selectedTimetable._id);
+    localStorage.setItem('name', selectedTimetable.name);
+    localStorage.setItem('courses', JSON.stringify(selectedTimetable.courses));
+    localStorage.setItem('isPublic', selectedTimetable.isPublic);
+    localStorage.setItem('userEmail', selectedTimetable.userEmail);
+    localStorage.setItem('description',selectedTimetable.description);
+    this.router.navigate(['edittimetable'])
+  }
+
+  
+navigateToTimetable() {
+  this.router.navigate(['search']);
+}
+
+navigateToMakeTimetable() {
+  this.router.navigate(['maketimetable']);
+}
+
+navigateToViewTimetable() {
+  this.router.navigate(['viewtimetable']);
+}
+
+
+}
